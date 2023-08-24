d4dc81f5d695abfaa4c4cec78c90a0bbaf286489
tb <thalisonbarbosa@outlook.com>
2023-08-23 22:47:32 -0300

    função para selecionar arquivo pdf e gerar arquivo de texto dentro da aplicação com o conteúdo dele

--- BibliotecaVirtual/src/AdicionarPDF.java
+++ BibliotecaVirtual/src/AdicionarPDF.java
@@ -1,44 +1,52 @@
-import org.apache.pdfbox.multipdf.PDFMergerUtility;
-import org.apache.pdfbox.pdmodel.PDDocument;
-
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
-import javax.swing.*;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 public class AdicionarPDF {
-    private static void addPDF(File arquivoSelecionado, File targetFile) throws IOException {
-        PDDocument sourceDoc = PDDocument.load(arquivoSelecionado);
-        PDDocument targetDoc = PDDocument.load("C:/Users/TB/Downloads/targetPDF.pdf");
-
-        PDFMergerUtility merger = new PDFMergerUtility();
-        merger.appendDocument(targetDoc, sourceDoc);
-
-        targetDoc.save(targetFile);
-        targetDoc.close();
-        sourceDoc.close();
-    }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         JFileChooser fileChooser = new JFileChooser();
-        
-        // Exibe o diálogo de seleção de arquivo
-        int result = fileChooser.showOpenDialog(null);
-        
-        if (result == JFileChooser.APPROVE_OPTION) {
+        int result = JOptionPane.showOptionDialog(null, fileChooser, "Selecione um arquivo", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
+
+        if (result == JOptionPane.OK_OPTION) {
             // Obtém o arquivo selecionado
-            File arquivoSelecionado = fileChooser.getSelectedFile();
-            File targetPDF = new File("target.pdf");
-            // Faça algo com o arquivo selecionado
+            File selectedFile = fileChooser.getSelectedFile();
+            
             try {
-                addPDF(arquivoSelecionado, targetPDF);
+                // Carregar o PDF
+                PDDocument pdf = PDDocument(selectedFile);
+
+                // Extrair o texto do PDF (apenas um exemplo, você pode fazer mais com o conteúdo)
+                PDFTextStripper pdfTextStripper = new PDFTextStripper();
+                String pdfText = pdfTextStripper.getText(pdf);
+
+                // Criar um novo arquivo dentro da aplicação
+                File newFile = new File("arquivo_novo.txt");
+                FileWriter fileWriter = new FileWriter(newFile);
+                fileWriter.write(pdfText);
+                fileWriter.close();
+
+                // Fechar o documento PDF
+                pdf.close();
+
+                System.out.println("Novo arquivo criado com sucesso.");
             } catch (IOException e) {
-                // TODO Auto-generated catch block
-                System.out.println("Erro ao adicionar livro!");
                 e.printStackTrace();
             }
-            System.out.println("Livro " + arquivoSelecionado.getAbsolutePath() + " adicionado!");
+
+            // Apresenta mensagem com resultado da ação
+            System.out.println("Arquivo selecionado: " + selectedFile.getAbsolutePath());
         } else {
-            System.out.println("Nenhum arquivo selecionado!");
+            System.out.println("Nenhum arquivo selecionado.");
         }
+    }
+
+    private static PDDocument PDDocument(File selectedFile) {
+        return null;
     }
 }
